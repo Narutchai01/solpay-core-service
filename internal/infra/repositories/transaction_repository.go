c456e611af3a302f70e8fd27927b95507c1d0c2c
nkmbxy <nakamon22547@gmail.com>
2026-01-31 13:54:40 +0700

    Add GetTransactionByAccountID method to TransactionRepository for retrieving transactions by account ID

--- internal/infra/repositories/transaction_repository.go
+++ internal/infra/repositories/transaction_repository.go
@@ -58,6 +58,8 @@ func (r *TransactionRepository) GetTransactionByAccountID(accountID int) ([]enti
 		}
 	}
 	return transactions, nil
+}
+
 func (r *TransactionRepository) GetTransactionByID(transactionID int) (*entities.TransactionEntity, error) {
 	var transaction entities.TransactionEntity
 	if err := r.db.First(&transaction, transactionID).Error; err != nil {
