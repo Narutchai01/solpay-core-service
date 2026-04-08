0c1fcde6ab719bbb5d0f2693c63634b6bfb049bb
nkmbxy <nakamon22547@gmail.com>
2026-04-08 12:32:15 +0700

    feat: implement admin authentication and profile management with JWT support

--- internal/routes/admin_route.go
+++ internal/routes/admin_route.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	"github.com/Narutchai01/solpay-core-service/internal/config"
 	"github.com/Narutchai01/solpay-core-service/internal/core/services"
 	"github.com/Narutchai01/solpay-core-service/internal/handler"
 	"github.com/Narutchai01/solpay-core-service/internal/infra/repositories"
+	"github.com/Narutchai01/solpay-core-service/internal/middlewares"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
@@ -21,9 +23,13 @@ func NewAdminRouteConfig(route fiber.Router, db *gorm.DB) *AdminRouteConfig {
 }
 
 func (arc *AdminRouteConfig) Setup() {
+	cfg := config.LoadConfig()
+
 	adminRepo := repositories.NewGormAdminRepository(arc.db)
-	adminService := services.NewAdminService(adminRepo)
+	adminService := services.NewAdminService(adminRepo, cfg)
 	adminHandler := handler.NewAdminHandler(adminService)
 
 	arc.route.Post("/", adminHandler.CreateAdminHandler)
+	arc.route.Post("/login", adminHandler.LoginAdminHandler)
+	arc.route.Get("/me", middlewares.JWTMiddleware(cfg), adminHandler.GetProfileHandler)
 }
