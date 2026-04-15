5718c441cc138f0b22bc6399b31fc7b30dc76107
Narutchai <koonfluk2015@gmail.com>
2026-04-16 01:12:01 +0700

    feat: enhance quote and balance response DTOs with additional fields

--- internal/dto/response/quote_response.go
+++ internal/dto/response/quote_response.go
@@ -5,6 +5,8 @@ type QuoteResponse struct {
 	THBAmount        float64 `json:"thb_amount"`
 	USDTAmount       float64 `json:"usdt_amount"`
 	ExchangeRate     float64 `json:"exchange_rate"`
+	PromptPayID      *string `json:"promptpay_id,omitempty"`
 	Fee              float64 `json:"fee"`
+	QuoteTpye        string  `json:"quote_type"`
 	ExpiresInSeconds int     `json:"expires_in_seconds"`
 }
